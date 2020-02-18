a37a41f6cf472234585def9089500644393a26ec
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-18 17:04:42 +0530

    Added solution function to solution file

--- solution.py
+++ solution.py
@@ -8,11 +8,34 @@ def getFileData(fileName):
     sliceCount , pizzaType = [int(ip) for ip in next(file).split()]
     for line in file:
         noOfSlices = [int(ip) for ip in line.split()]
-    f.close()
+    file.close()
     return(sliceCount,pizzaType,noOfSlices)
 
+def getRequiredPizza(sliceCount,pizzaCount,slices):
+    maxNeed = 0
+    totalSlices = []
+    for i in range(len(slices)-1,-1,-1):
+        currentNeed = 0
+        currentSlices = []
+        for j in range(i,-1,-1):
+            currentSlice = slices[j]
+            totalNeed = currentNeed + currentSlice
+            if totalNeed == sliceCount:
+                currentNeed = totalNeed
+                currentSlices.append(j)
+                break
+            elif totalNeed < sliceCount:
+                currentNeed = totalNeed
+                currentSlices.append(j)
+                continue
+        if maxNeed < currentNeed:
+            maxNeed = currentNeed
+            totalSlices = currentSlices
+    return totalSlices
 
 files = getFiles()
 
 for fileName in files:
-    fileData = getFileData(files[0])
+    fileData = getFileData(fileName)
+    result = getRequiredPizza(fileData[0],fileData[1],fileData[2])
+    print(len(result))
