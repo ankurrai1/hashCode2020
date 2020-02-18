fd1560e3775c1bf618e86aba0f82032707a998a2
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-18 15:42:40 +0530

    Added data extractor function to solution

--- solution.py
+++ solution.py
@@ -3,7 +3,16 @@ import glob
 def getFiles():
     return glob.glob("./input/*.in")
 
+def getFileData(fileName):
+    f= open(fileName, 'r')
+    sliceCount , pizzaType = [int(ip) for ip in next(f).split()]
+    for line in f:
+        noOfSlices = [int(x) for x in line.split()]
+    f.close()
+    return(sliceCount,pizzaType,noOfSlices)
+
+
 files = getFiles()
 
 for fileName in files:
-    print(fileName)
+    fileData = getFileData(files[0])
